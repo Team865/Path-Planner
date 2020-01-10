355ec520c1e48de269beb63c1b6f24a410e0235f
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2020-01-09 22:50:10 -0500

    fix jlink merging errors

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,7 +4,7 @@ module path.planner {
     requires javafx.controls;
     requires wpilibj.java;
     requires wpiutil.java;
-    requires com.fasterxml.jackson.core;
+//    requires ejml.combined;
 
     exports ca.warp7.planner2;
     exports ca.warp7.frc2020.lib.trajectory;
